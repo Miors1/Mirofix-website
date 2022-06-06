8fefc75470c0057c1ef724f58cde1f5693df7968
Łukasz Mirowicz <lukasm123@gmail.com>
2022-06-06 23:11:48 +0200

    added offset for scrollspy, changed js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
-document.addEventListener('DOMContentLoaded',function(){
+const nav = document.querySelector('.navbar-collapse');
 
-    const nav=document.querySelector('.navbar')
-    const allNavItems = document.querySelectorAll('.nav-link') // wszystkie linki
-    const navList = document.querySelector('.navbar-collapse')  
-
-    
-// klikniecie w liste powoduje jej zamkniecie
-allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
-
-
-
-window.addEventListener('scroll',addShadow)
-
-})
-
-
-
+document.addEventListener('click', () => {
+	if (nav.classList.contains('show')) {
+		nav.classList.remove('show');
+	}
+});
